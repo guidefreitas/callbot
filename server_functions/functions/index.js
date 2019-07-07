c2bc69b12d22c825bdc5c2435bf3cf02f467f045
Guilherme Defreitas <guilherme.defreitas@gmail.com>
2019-07-07 18:10:07 -0300

    Refatora e adiciona comando who para saber quem ativou o callbot

--- server_functions/functions/index.js
+++ server_functions/functions/index.js
@@ -31,4 +31,56 @@ exports.deactivate = functions.https.onRequest((request, response) => {
   });
   response.send("ok");
 });
+
+function getCommandHelp() {
+  return `O comando deve ser:
+/callbot start - Inicia o callbot
+/callbot stop - Encerra o callbot
+/callbot who - Mostra quem está em call 
+`;
+}
+
+function startCallbot(username) {
+  admin.database().ref('/call').set({
+    status: 1,
+    username: username,
+  });
+}
+
+function stopCallbot() {
+  admin.database().ref('/call').set({
+    status: 0,
+    username: '',
+  });
+}
+
+function verifyCaller(response) {
+  admin.database().ref('/call').once('value', (snapshot) => {
+    if(snapshot.val().status === 1) {
+      response.send(`Shh! O mochileiro(a) ${snapshot.val().username} está em call!`);
+    } else {
+      response.send('Ninguém em call');
+    }
+  });
+}
+
+exports.call = functions.https.onRequest((request, response) => {
+
+  var commandText =  request.body.text;
+
+  switch(commandText) {
+    case 'start': startCallbot(request.body.user_name);
+                  response.send('Callbot ligado');
+                  break;
+    
+    case 'stop': stopCallbot();
+                 response.send('Callbot desligado');
+                 break;
+    
+    case 'who': verifyCaller(response);
+                break;
+    
+    default: response.send(getCommandHelp());
+  }
+});
  
