04cd52df2b11e7b90e600454f289eeab7c718a9b
Guilherme Defreitas <guilherme.defreitas@gmail.com>
2019-07-09 14:06:35 -0300

    Adiciona scheduler para enviar mensagem a pessoa que ativou o callbot

--- server_functions/functions/index.js
+++ server_functions/functions/index.js
@@ -1,6 +1,10 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const request = require('request');
+
 admin.initializeApp(functions.config().firebase);
+
+const FIREBASE_URL = 'https://hooks.slack.com/services/...';
 
 exports.status = functions.https.onRequest((request, response) => {
   admin.database().ref('/call').once('value', (snapshot) => {
@@ -21,6 +25,8 @@ exports.status_str = functions.https.onRequest((request, response) => {
 exports.activate = functions.https.onRequest((request, response) => {
   admin.database().ref('/call').set({
     status: 1,
+    username: '',
+    activatedAt: null,
   });
   response.send("ok");
 });
@@ -28,6 +34,8 @@ exports.activate = functions.https.onRequest((request, response) => {
 exports.deactivate = functions.https.onRequest((request, response) => {
   admin.database().ref('/call').set({
     status: 0,
+    username: '',
+    activatedAt: null,
   });
   response.send("ok");
 });
@@ -44,6 +52,7 @@ function startCallbot(username) {
   admin.database().ref('/call').set({
     status: 1,
     username: username,
+    activatedAt: Date.now(),
   });
 }
 
@@ -51,13 +60,52 @@ function stopCallbot() {
   admin.database().ref('/call').set({
     status: 0,
     username: '',
+    activatedAt: null,
   });
 }
+
+function sendSlackMessage(data) {
+  const msgPayload = {
+    channel: `@${data.username}`,
+    as_user: 'callbot',
+    text: 'Você ainda está em call? Se não estiver, lembre-se de desativar o callbot com /callbot stop'
+  }
+
+  const payloadJson = JSON.stringify(msgPayload);
+
+  const options = {
+    url: FIREBASE_URL,
+    headers: {
+       'Content-Type': 'application/json',
+    },
+    body: payloadJson,
+  }
+
+  request.post(options);
+}
+
+function notifyCaller(data) {
+  if(data.status === 1 && data.activatedAt !== null) {
+      const activatedHours = new Date(data.activatedAt).getHours();
+      const currentHours = new Date().getHours();
+
+      if(currentHours - activatedHours > 1) {
+        sendSlackMessage(data);
+      }
+  }
+}
+
+exports.scheduledFunction = functions.pubsub.schedule('every 20 minutes').onRun((context) => {
+  admin.database().ref('/call').once('value', (snapshot) => {
+    notifyCaller(snapshot.val());
+  });
+});
 
 function verifyCaller(response) {
   admin.database().ref('/call').once('value', (snapshot) => {
     if(snapshot.val().status === 1) {
-      response.send(`Shh! O mochileiro(a) ${snapshot.val().username} está em call!`);
+      const username = snapshot.val().username;
+      response.send(`Shh! O mochileiro(a) ${username} está em call!`);
     } else {
       response.send('Ninguém em call');
     }
